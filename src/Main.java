ab5148acaefb4dff607c0b97f967435d40599f9a
Pavlo <qsplweroiucvbnm541@gmail.com>
2023-12-10 23:39:58 +0200

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import battle.Battle;
-import droids.Droid;
-import droids.type.AirDroid;
-import droids.type.AttackDroid;
-
-import java.util.ArrayList;
-
 import static menu.Menu.runMenu;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         runMenu();
